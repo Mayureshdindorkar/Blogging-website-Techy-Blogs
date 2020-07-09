c3c998eba748d39e0b222ac078b92996bb4aa141
Mayuresh Dindorkar <dmayuresh99@gmail.com>
2020-07-09 16:54:15 +0530

    after collect static

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7f44ohfjlo@%y&b^8*0zu1si@=w5k!nl#6401x5h7c9o($0c9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
